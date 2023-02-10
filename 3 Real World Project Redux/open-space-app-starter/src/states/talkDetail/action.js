abaee91967094c00835bd9a1d6e02a84ce6510a8
Aditya Negara <aditnegarabisnis@gmail.com>
2023-02-10 14:10:36 +0700

    Finish practice action and reducer for talks

--- 3 Real World Project Redux/open-space-app-starter/src/states/talkDetail/action.js
+++ 3 Real World Project Redux/open-space-app-starter/src/states/talkDetail/action.js
@@ -1,3 +1,29 @@
-/**
- * @TODO: Define all the actions (creator) for the talkDetail state
- */
+const ActionType = {
+  RECEIVE_TALK_DETAIL: 'RECEIVE_TALK_DETAIL',
+  CLEAR_TALK_DETIAL: 'CLEAR_TALK_DETAIL',
+  TOGGLE_LIKE_TALK_DETAIL: 'TOGGLE_LIKE_TALK_DETAIL',
+};
+
+function receiveTalkDetailActionCreator(talkDetail) {
+  return {
+    type: ActionType.RECEIVE_TALK_DETAIL,
+    payload: {
+      talkDetail,
+    },
+  };
+}
+
+function clearTalkDetailActionCreator() {
+  return {
+    type: ActionType.CLEAR_TALK_DETIAL,
+  };
+}
+
+function toggleLikeTalkDetailActionCreator(userId) {
+  return {
+    type: ActionType.TOGGLE_LIKE_TALK_DETAIL,
+    payload: {
+      userId,
+    },
+  };
+}
